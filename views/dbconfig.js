70f65f0a771d060b53833f19d306e5a1e0fc38d6
ARUL D <aruln311@gmail.com>
2022-04-02 00:41:46 +0530

    Update dbconfig.js

--- views/dbconfig.js
+++ views/dbconfig.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 dbName = 'sample1'
-const dbUrl = `mongodb+srv://Arul03:arul0311@cluster0.8pfx9.mongodb.net/${dbName}`
+const dbUrl = `mongodb+srv://Arul:arul@cluster0.8pfx9.mongodb.net/${dbName}`
 module.exports={dbUrl,mongodb,MongoClient}
